105ed537c7535c5f03aee738d082565a02481511
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-04-27 01:46:43 +0600

    u: Shelter Component

--- front/Adopt-Me/src/app/services/shelters.service.ts
+++ front/Adopt-Me/src/app/services/shelters.service.ts
@@ -10,22 +10,24 @@ import {Shelter} from "../models/shelters";
 export class SheltersService {
 
   private BASE_URL: string;
+  //temp
+  private shelters: Shelter[];
 
   constructor(private http: HttpClient) {
     this.BASE_URL = COMMON_BASE_URL + 'shelters/';
-  }
-
-  public getAllShelters(): Observable<Shelter[]>{
-    // return this.http.get<Shelter[]>(this.BASE_URL);
-    const shelters = [
+    this.shelters =  [
       {id: 1, name: 'X', city: 'Almaty', country: 'Kazakhstan'},
       {id: 2, name: 'Y', city: 'Astana', country: 'Kazakhstan'},
       {id: 3, name: 'Z', city: 'Shymkent', country: 'Kazakhstan'},
     ];
+  }
+
+  public getAllShelters(): Observable<Shelter[]>{
+    // return this.http.get<Shelter[]>(this.BASE_URL);
 
     return new Observable<Shelter[]>(observer => {
       setTimeout(() => {
-        observer.next(shelters);
+        observer.next(this.shelters);
       }, 1000);
     });
   }
@@ -35,14 +37,47 @@ export class SheltersService {
   }
 
   public addShelter(shelter: Shelter): Observable<Shelter> {
-    return this.http.post<Shelter>(`${this.BASE_URL}`, shelter);
+    // return this.http.post<Shelter>(`${this.BASE_URL}`, shelter);
+    this.shelters.push(shelter);
+
+    return new Observable<Shelter>(observer => {
+      setTimeout(() => {
+        observer.next(shelter);
+      }, 1000);
+    });
   }
 
   public updateShelter(id: number, shelter: Shelter): Observable<Shelter> {
-    return this.http.put<Shelter>(`${this.BASE_URL}${id}`, shelter);
+    // return this.http.put<Shelter>(`${this.BASE_URL}${id}`, shelter);
+
+    for(let i = 0; i < this.shelters.length; i++){
+      if(this.shelters[i].id === shelter.id){
+        this.shelters[i] = shelter;
+        break;
+      }
+    }
+
+    return new Observable<Shelter>(observer => {
+      setTimeout(() => {
+        observer.next(shelter);
+      }, 1000);
+    });
   }
 
   public deleteShelter(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.BASE_URL}${id}`);
+    // return this.http.delete<any>(`${this.BASE_URL}${id}`);
+
+    for(let i = 0; i < this.shelters.length; i++){
+      if(this.shelters[i].id === id){
+        this.shelters.splice(i, 1);
+        break;
+      }
+    }
+
+    return new Observable<number>(observer => {
+      setTimeout(() => {
+        observer.next(1);
+      }, 1000);
+    });
   }
 }
