53865455d063328b3df1af7be265735b02f77a25
zhonniks <zh_omarova@kbtu.kz>
2023-04-26 22:40:57 +0600

    Updated routing,created header

---
+++ front/Adopt-Me/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { PetsComponent } from './pets/pets.component';
+import { SheltersComponent } from './shelters/shelters.component';
+
+const routes: Routes = [
+    { path : 'home',component:WelcomePageComponent},
+    { path : 'pets', component: PetsComponent},
+    { path : 'shelters', component: SheltersComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- front/Adopt-Me/src/app/app.module.ts
+++ front/Adopt-Me/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {PetsComponent} from "./pets/pets.component";
 import { SheltersComponent } from './shelters/shelters.component';
+import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
+import {HeaderComponent} from "./header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomePageComponent,
     PetsComponent,
+    HeaderComponent,
     SheltersComponent,
   ],
   imports: [
