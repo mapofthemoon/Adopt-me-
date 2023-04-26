09587aaaa6375bc93137dea9cf582b2de3c6d1d0
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-04-27 00:03:49 +0600

    u: Shelter Component

--- front/Adopt-Me/src/app/shelters/shelters.component.ts
+++ front/Adopt-Me/src/app/shelters/shelters.component.ts
@@ -9,18 +9,18 @@ import {SheltersService} from "../services/shelters.service";
 })
 export class SheltersComponent implements OnInit{
   shelters: Shelter[];
-  shelters_not_founded: boolean;
+  shelters_not_found: boolean;
 
   constructor(private sheltersService: SheltersService) {
     this.shelters = [];
-    this.shelters_not_founded = false;
+    this.shelters_not_found = false;
   }
 
   ngOnInit(): void {
     this.sheltersService.getAllShelters().subscribe((shelters) => {
       this.shelters = shelters;
     }, error => {
-      this.shelters_not_founded = true;
+      this.shelters_not_found = true;
     });
   }
 
