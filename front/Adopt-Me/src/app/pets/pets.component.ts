b37729f0395d92dc6309171afad7ea2cf6060283
zhonniks <zh_omarova@kbtu.kz>
2023-04-26 20:16:49 +0600

    Added components Pets,Shelters

--- front/Adopt-Me/src/app/pets/pets.component.ts
+++ front/Adopt-Me/src/app/pets/pets.component.ts
@@ -1,13 +1,10 @@
-import { Component , OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
     selector: 'app-pets',
     templateUrl: './pets.component.html',
     styleUrls: ['./pets.component.css']
 })
-export class PetsComponent implements OnInit{
-    constructor() {
-    }
 
-    ngOnInit(): void {
-    }
+export class PetsComponent {
+
 }
