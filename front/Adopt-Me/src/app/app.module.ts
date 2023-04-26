8a2a41068bfa55314c76ab26d72c269085c4fb53
zhonniks <zh_omarova@kbtu.kz>
2023-04-26 20:15:36 +0600

    Added components Pets,Shelters

--- front/Adopt-Me/src/app/app.module.ts
+++ front/Adopt-Me/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {PetsComponent} from "./pets/pets.component";
+import { SheltersComponent } from './shelters/shelters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetsComponent,
+    SheltersComponent,
   ],
   imports: [
     BrowserModule
