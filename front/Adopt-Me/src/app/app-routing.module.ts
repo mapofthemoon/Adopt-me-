15f55c2e24f0b60b2a8684f47965866a949afda7
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-04-27 11:30:02 +0600

    u: Volunteers component

--- front/Adopt-Me/src/app/app-routing.module.ts
+++ front/Adopt-Me/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { PetsComponent } from './pets/pets.component';
 import { SheltersComponent } from './shelters/shelters.component';
+import {VolunteersComponent} from "./volunteers/volunteers.component";
 
 const routes: Routes = [
     { path : 'home',component:WelcomePageComponent},
     { path : 'pets', component: PetsComponent},
     { path : 'shelters', component: SheltersComponent},
-    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path : 'volunteers', component: VolunteersComponent},
+    { path : '', redirectTo: 'home', pathMatch: 'full'},
 ];
 
 @NgModule({
