7243edb27ffac4963e1d3ea1fed43444c368642f
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-05-03 15:28:54 +0600

    auth

--- front/Adopt-Me/src/app/services/auth.service.ts
+++ front/Adopt-Me/src/app/services/auth.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {COMMON_BASE_URL} from "./source";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient, private router: Router) { }
+  private BASE_URL: string;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.BASE_URL = COMMON_BASE_URL;
+  }
+
+  public login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(
+        `${this.BASE_URL}`,
+        {},
+        {
+          params: {
+            ['email']: email,
+            ['password']: password
+          }
+        }
+        );
+  }
+
+
 }
