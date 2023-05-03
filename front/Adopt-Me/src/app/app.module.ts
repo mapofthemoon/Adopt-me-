89c84aa2fb48eabd2c4350118d4968602fc39127
ual1prekrasneyshyy <uali03.alm@gmail.com>
2023-05-03 15:00:30 +0600

    auth

--- front/Adopt-Me/src/app/app.module.ts
+++ front/Adopt-Me/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
 import {FormsModule} from "@angular/forms";
 import { VolunteersComponent } from './volunteers/volunteers.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { VolunteersComponent } from './volunteers/volunteers.component';
     HeaderComponent,
     SheltersComponent,
     VolunteersComponent,
+    LoginComponent,
   ],
     imports: [
         BrowserModule,
